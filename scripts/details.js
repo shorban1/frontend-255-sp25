5113974d2b8ff87a2ab49ac215366afd559f7d46
shorban1 <shorban1@ivytech.edu>
2025-02-27 12:25:33 -0500

    Update details.js

--- scripts/details.js
+++ scripts/details.js
@@ -4,7 +4,7 @@ addEventListener("DOMContentLoaded", async function () {
   console.log(songID);
 
   const response = await fetch(
-    "https://right-horn-flamingo.glitch.me/api/songs" + songID
+    "https://right-horn-flamingo.glitch.me/api/songs/" + songID
   );
   const song = await response.json();
   console.log(song);
